53430aff784e480e624e991803062c6944936ba8
Vitor Costa Jose <vitor.costa@prontmed.com>
2021-04-13 17:33:01 -0300

    add: extra content

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const doctorModel = require("../models/doctor");
+const patientModel = require("../models/patient");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.render("attendance");
+router.get("/", async function (req, res, next) {
+  const doctors = await doctorModel.getDoctors();
+  const patients = await patientModel.getPatient();
+
+  res.render("attendance", { doctors: doctors, patients: patients });
 });
+
+router.post("/", async function(req, res) {
+  // cadastrar no banco de dados
+  res.redirect("/attendance");
+})
 
 module.exports = router;
