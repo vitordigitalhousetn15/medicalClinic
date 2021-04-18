2d8a7125d18ccbbc3fa2b63613707c783ceb6a78
Vitor Costa Jose <vitor.costa@prontmed.com>
2021-04-18 18:11:37 -0300

    add: summary js code

--- public/javascripts/patient.js
+++ public/javascripts/patient.js
@@ -10,3 +10,29 @@ console.log(totalPatients);
 
 const totalTableItems = document.querySelectorAll("table tbody tr").length;
 totalPatients.innerHTML = totalPatients.innerHTML + " " + totalTableItems;
+
+const genders = document.querySelectorAll('table tbody tr td.gender');
+
+let countM = 0;
+let countF = 0;
+
+genders.forEach(function(item) {
+  if (item.innerHTML === 'F') {
+    countF = countF + 1;
+  }
+
+  if (item.innerHTML === 'M') {
+    countM = countM + 1;
+  }
+})
+
+// outra opção document.querySelector('#man-count')
+document.getElementById('man-count').innerHTML =
+  document.getElementById('man-count').innerHTML + countM;
+
+// outra opção document.querySelector('#woman-count')
+document.getElementById('woman-count').innerHTML =
+  document.getElementById('woman-count').innerHTML + countF;
+
+console.log('Masculino', countM);
+console.log('Feminino', countF);
