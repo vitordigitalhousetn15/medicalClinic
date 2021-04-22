f9f68b4f5aac6a576ccf689e808d0b86e941499e
Vitor Costa Jose <vitor.costa@prontmed.com>
2021-04-22 15:26:05 -0300

    add: validation to patient's form fields

--- public/javascripts/patient.js
+++ public/javascripts/patient.js
@@ -11,28 +11,75 @@ console.log(totalPatients);
 const totalTableItems = document.querySelectorAll("table tbody tr").length;
 totalPatients.innerHTML = totalPatients.innerHTML + " " + totalTableItems;
 
-const genders = document.querySelectorAll('table tbody tr td.gender');
+const genders = document.querySelectorAll("table tbody tr td.gender");
 
 let countM = 0;
 let countF = 0;
 
-genders.forEach(function(item) {
-  if (item.innerHTML === 'F') {
+genders.forEach(function (item) {
+  if (item.innerHTML === "F") {
     countF = countF + 1;
   }
 
-  if (item.innerHTML === 'M') {
+  if (item.innerHTML === "M") {
     countM = countM + 1;
   }
-})
+});
 
 // outra opção document.querySelector('#man-count')
-document.getElementById('man-count').innerHTML =
-  document.getElementById('man-count').innerHTML + countM;
+document.getElementById("man-count").innerHTML =
+  document.getElementById("man-count").innerHTML + countM;
 
 // outra opção document.querySelector('#woman-count')
-document.getElementById('woman-count').innerHTML =
-  document.getElementById('woman-count').innerHTML + countF;
+document.getElementById("woman-count").innerHTML =
+  document.getElementById("woman-count").innerHTML + countF;
 
-console.log('Masculino', countM);
-console.log('Feminino', countF);
+totalPatients.addEventListener("dblclick", function () {
+  totalPatients.classList.add("text-danger");
+});
+
+totalPatients.addEventListener("mouseover", function () {
+  totalPatients.classList.add("text-success");
+});
+
+totalPatients.addEventListener("mouseout", function () {
+  totalPatients.classList.remove("text-success");
+});
+
+const inputFormName = document.querySelector("#register-patient #name");
+const inputFormBirthDate = document.querySelector(
+  "#register-patient #birth_date"
+);
+const inputFormDocument = document.querySelector("#register-patient #document");
+const inputFormGender = document.querySelector("#register-patient #gender");
+const alertDanger = document.querySelector(".alert-danger");
+
+function onInvalid(inputForm, liId) {
+  alertDanger.classList.remove("d-none");
+
+  if (inputForm.value === "") {
+    document.getElementById(liId).classList.remove("d-none");
+  } else {
+    document.getElementById(liId).classList.add("d-none");
+  }
+}
+
+inputFormName.addEventListener("keyup", function () {
+  document.getElementById("invalid-name").classList.add("d-none");
+});
+
+inputFormName.addEventListener("invalid", function () {
+  onInvalid(inputFormName, "invalid-name");
+});
+
+inputFormGender.addEventListener("invalid", function () {
+  onInvalid(inputFormGender, "invalid-gender");
+});
+
+inputFormBirthDate.addEventListener("invalid", function () {
+  onInvalid(inputFormGender, "invalid-birth-date");
+});
+
+inputFormDocument.addEventListener("invalid", function () {
+  onInvalid(inputFormGender, "invalid-document");
+});
